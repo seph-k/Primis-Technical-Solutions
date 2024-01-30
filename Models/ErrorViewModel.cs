8820e05dd7c578ba870d61a1202bb087c8865bd1
seph2021 <88113379+seph-ora@users.noreply.github.com>
2024-01-30 10:21:58 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Primis_Technical_Solutions.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
